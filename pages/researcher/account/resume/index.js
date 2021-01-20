dfa3ffa6ca60eb1a1d4c7306921447a0d5a08dc9
adel <mohnagato@gmail.com>
2021-01-20 12:46:29 +0100

    extarnalization of successSection

--- pages/researcher/account/resume/index.js
+++ pages/researcher/account/resume/index.js
@@ -1,16 +1,8 @@
 import ResearcherAccountLayout from '../../../../layouts/ResearcherAccountLayout/ResearcherAccountLayout'
-import React,{useState} from 'react'
-import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
-import { Button, IconButton, Tab,Tabs } from '@material-ui/core';
-import BorderColorOutlinedIcon from '@material-ui/icons/BorderColorOutlined';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import WorkOutlineOutlinedIcon from '@material-ui/icons/WorkOutlineOutlined';
 import SchoolOutlinedIcon from '@material-ui/icons/SchoolOutlined';
 import CardMembershipOutlinedIcon from '@material-ui/icons/CardMembershipOutlined';
 import classes from '../../../../styles/Resume.module.css'
-import GroupAddOutlinedIcon from '@material-ui/icons/GroupAddOutlined';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import AboutMe from '../../../../components/AboutMe/AboutMe';
 import BadgesCard from '../../../../components/BadgesCard/BadgesCard';
 import MyNetwork from '../../../../components/MyNetwork/MyNetwork';
@@ -20,6 +12,7 @@ import MyGroups from '../../../../components/MyGroups/MyGroups';
 import ResumeMainCollection from '../../../../components/ResumeMainCollection/ResumeMainCollection';
 import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ResumeSuccessItem from '../../../../components/ResumeSuccessItem/ResumeSuccessItem';
 
 const aboutme = {
     description:"لوريم ايبسوم هو نموذج افتراضي يوضع في التصاميم لتعرض على العميل ليتصور طريقه وضع النصوص بالتصاميم سواء كانت تصاميم مطبوعه ... بروشور او فلاير على سبيل.",
@@ -356,30 +349,24 @@ const honors = [
     {
         name:"التكريم الفلاني الأول",
         date:"ديسمبر 2012",
-        honoredBy:"مركز الرؤيا للأبحاث الفلكية",
+        center:"مركز الرؤيا للأبحاث الفلكية",
         description:"خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين "
     },
     {
         name:"التكريم الفلاني الأول",
         date:"ديسمبر 2012",
-        honoredBy:"مركز الرؤيا للأبحاث الفلكية",
+        center:"مركز الرؤيا للأبحاث الفلكية",
         description:"خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين "
     },
     {
         name:"التكريم الفلاني الأول",
         date:"ديسمبر 2015",
-        honoredBy:"مركز الرؤيا للأبحاث الفلكية",
+        center:"مركز الرؤيا للأبحاث الفلكية",
         description:"خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين "
     },
 ]
 
 export default function index() {
-
-    const [projectsExpanded,setProjectsExpanded] = useState(false)
-    const [languagesExpanded,setLanguagesExpanded] = useState(false)
-    const [activitiesExpanded,setActivitiesExpanded] = useState(false)
-    const [patentsExpanded,setPatentsExpanded] = useState(false)
-    const [honorsExpanded,setHonorsExpanded] = useState(false)
     
     return (
        <ResearcherAccountLayout>
@@ -394,211 +381,14 @@ export default function index() {
                <ResumeMainCollection icon={<WorkOutlineOutlinedIcon />} label="الخبرة المهنية" collections={experiences} />
                <ResumeMainCollection icon={<CardMembershipOutlinedIcon />} label="الشهادات" collections={certifications} />
                <ResumeMainCollection icon={<FontAwesomeIcon icon={faHandHoldingHeart} style={{fontSize:"1.5rem"}} />} label="العمل التطوعي" collections={volunteerings} />
-               <div className={classes.collectionContainer}>
-                    <h2>
-                        <span>
-                        <i className="fas fa-award" style={{fontSize:"1.5rem"}}></i>
-                            الإنجازات
-                        </span>
-                        <IconButton>
-                            <BorderColorOutlinedIcon className={classes.actionSectionIcon}/>
-                        </IconButton>
-                    </h2>
-                    <div className={classes.successSection}>
-                            <h3>
-                                المشاريع
-                                <IconButton onClick={()=>setProjectsExpanded(!projectsExpanded)}>
-                                    {
-                                        projectsExpanded ?  <ExpandLessIcon style={{fontSize:28,color:"#3e3f5e"}} />: <ExpandMoreIcon style={{fontSize:28,color:"#3e3f5e"}} /> 
-                                    }
-                                </IconButton>
-                            </h3>
-                            {
-                                projectsExpanded ? (
-                                    <ul className={classes.successSectionNotExpanded}>
-                                        {
-                                            projects.map((project,index)=>(
-                                                <li key={`project-${index}`}>{project.name}</li>
-                                            ))
-                                        }
-                                    </ul>
-                                ):(
-                                    <div className={classes.successSectionItems}>
-                                        {
-                                            projects.map((project,index)=>(
-                                                <div key={`project-${index}`} className={classes.successSectionItem}>
-                                                    <h3>{project.name}</h3>
-                                                    <h4>
-                                                        {project.startDate} - {project.endDate!==null?project.endDate :"مستمر"}
-                                                    </h4>
-                                                    <span>{project.description}</span>
-                                                </div>
-
-                                            ))
-                                        }
-                                    </div>
-
-                                )
-                            }
-                            <div className={classes.seccessSectionDevider}></div>
-                    </div>
-                    <div className={classes.successSection}>
-                            <h3>
-                                اللغات
-                                <IconButton onClick={()=>setLanguagesExpanded(!languagesExpanded)}>
-                                {
-                                        languagesExpanded ?  <ExpandLessIcon style={{fontSize:28,color:"#3e3f5e"}} />: <ExpandMoreIcon style={{fontSize:28,color:"#3e3f5e"}} /> 
-                                    }
-                                </IconButton>
-                            </h3>
-                            {
-                                languagesExpanded ? (
-                                    <ul className={classes.successSectionNotExpanded}>
-                                        {languages.map((language,index)=>(
-                                            <li key={`language-${index}`}>{language.name}</li>
-                                        ))}
-                                    </ul>
-                                ) :(
-                                    <div className={classes.successSectionItems}>
-                                        {
-                                            languages.map((language,index)=>(
-                                                <div key={`language-${index}`} className={classes.successSectionItem}>
-                                                    <h3>{language.name}</h3>
-                                                    <h4>
-                                                        {language.level}
-                                                    </h4>
-                                                </div>
-
-                                            ))
-                                        }
-                                    </div>
-
-                                )   
-                            }
-                            <div className={classes.seccessSectionDevider}></div>
-                    </div>
-                    <div className={classes.successSection}>
-                            <h3>
-                                النشاطات والفعاليات
-                                <IconButton onClick={()=>setActivitiesExpanded(!activitiesExpanded)}>
-                                    {
-                                        activitiesExpanded ?  <ExpandLessIcon style={{fontSize:28,color:"#3e3f5e"}} />: <ExpandMoreIcon style={{fontSize:28,color:"#3e3f5e"}} /> 
-                                    }
-                                </IconButton>
-                            </h3>
-                            {
-                                activitiesExpanded ? (
-                                    <ul className={classes.successSectionNotExpanded}>
-                                        {
-                                            activities.map((activity,index)=>(
-                                                <li key={`activity-${index}`}>{activity.name}</li>
-                                            ))
-                                        }
-                                    </ul>
-                                ):(
-                                   
-                                    <div className={classes.successSectionItems}>
-                                        {
-                                            activities.map((activity,index)=>(
-                                                <div key={`activity-${index}`} className={classes.successSectionItem}>
-                                                    <h3>{activity.name}</h3>
-                                                    <h4>
-                                                        {activity.startDate} - {activity.endDate!==null?activity.endDate :"مستمر"} - {activity.role}
-                                                    </h4>
-                                                    <h4>
-                                                        {activity.center} - {activity.location}
-                                                    </h4>
-                                                </div>
-
-                                            ))
-                                        }
-                                    </div>
-                                )
-                            }
-                            <div className={classes.seccessSectionDevider}></div>
-                    </div>
-                    <div className={classes.successSection}>
-                            <h3>
-                                براءات الاختراع
-                                <IconButton onClick={()=>setPatentsExpanded(!patentsExpanded)}>
-                                {
-                                    patentsExpanded ?  <ExpandLessIcon style={{fontSize:28,color:"#3e3f5e"}} />: <ExpandMoreIcon style={{fontSize:28,color:"#3e3f5e"}} /> 
-                                }
-                                </IconButton>
-                            </h3>
-                            {
-                                patentsExpanded ? (
-                                <ul className={classes.successSectionNotExpanded}>
-                                    {
-                                        patents.map((patent,index)=>(
-                                            <li key={`patent-${index}`}>{patent.name}</li>
-                                        ))
-                                    }
-                                </ul>
-                                ):(
-                                    <div className={classes.successSectionItems}>
-                                        {
-                                            patents.map((patent,index)=>(
-                                                <div key={`patent-${index}`} className={classes.successSectionItem}>
-                                                    <h3>{patent.name}</h3>
-                                                    <h4>
-                                                        {patent.startDate} - {patent.endDate!==null?patent.endDate :"مستمر"} - {patent.status}
-                                                    </h4>
-                                                    <h4>
-                                                        {patent.center} - {patent.code}
-                                                    </h4>
-                                                    <span>{patent.description}</span>
-                                                </div>
-
-                                            ))
-                                        }
-                                    </div>
-
-                                )
-                            }
-                            <div className={classes.seccessSectionDevider}></div>
-                    </div>
-                    <div className={classes.successSection}>
-                            <h3>
-                                التكريمات
-                                <IconButton onClick={()=>setHonorsExpanded(!honorsExpanded)}>
-                                {
-                                   honorsExpanded ?  <ExpandLessIcon style={{fontSize:28,color:"#3e3f5e"}} />: <ExpandMoreIcon style={{fontSize:28,color:"#3e3f5e"}} /> 
-                                }
-                                </IconButton>
-                            </h3>
-                            {
-                                honorsExpanded ? (
-                                <ul className={classes.successSectionNotExpanded}>
-                                    {
-                                        honors.map((honor,index)=>(
-                                            <li key={`honor-${index}`}>{honor.name}</li>
-                                        ))
-                                    }
-                                </ul>
-                                ):(
-                                    <div className={classes.successSectionItems}>
-                                        {
-                                            honors.map((honor,index)=>(
-                                                <div key={`honor-${index}`} className={classes.successSectionItem}>
-                                                    <h3>{honor.name}</h3>
-                                                    <h4>
-                                                        {honor.date}
-                                                    </h4>
-                                                    <h4>
-                                                        {honor.honoredBy} 
-                                                    </h4>
-                                                    <span>{honor.description}</span>
-                                                </div>
-
-                                            ))
-                                        }
-                                    </div>
-
-                                )
-                            }
-                    </div>                   
-               </div>
+               <ResumeMainCollection label="الإنجازات">
+                    <ResumeSuccessItem label="المشاريع" items={projects} />
+                    <ResumeSuccessItem label="اللغات" items={languages} />
+                    <ResumeSuccessItem label="النشاطات و الفعاليات" items={activities} />
+                    <ResumeSuccessItem label="براءات الاختراع" items={patents} />
+                    <ResumeSuccessItem label="التكريمات" items={honors} last />                  
+                </ResumeMainCollection> 
+               
 
             </div>
             <div className={classes.sideSection}>
