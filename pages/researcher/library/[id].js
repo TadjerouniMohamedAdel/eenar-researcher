22c1f5ea792a1a97d126395186abe95cd4e758d3
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-02-18 15:01:46 +0100

    add swiper book to library book item

--- pages/researcher/library/[id].js
+++ pages/researcher/library/[id].js
@@ -6,9 +6,24 @@ import classes from '../../../styles/Library.module.css'
 import GetAppIcon from '@material-ui/icons/GetApp';
 import MyHead from '../../../components/MyHead/MyHead'
 import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
+import Swiper from 'react-id-swiper';
+import 'swiper/swiper-bundle.min.css';
+import SwiperCore, { Lazy, Navigation, Autoplay } from 'swiper'
+SwiperCore.use([Lazy, Navigation, Autoplay])
+
+
 
 export default function bookItemPage() {
     const [book,setBook] = useState({id:1,author:"آرثر كونان دويل",overview:"لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود     أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم فينايم,كيواس نوستريد كسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس  أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم أيو فيجايت    نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت ان     كيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم.سيت يتبيرسبايكياتيس يوندي أومنيس أستي ناتيس أيررور سيت فوليبتاتيم أكيسأنتييومدولاريمكيو لايودانتيوم,توتام ريم أبيرأم,أيكيو أبسا كيواي أب أللو أنفينتوري فيرأتاتيس ايتكياسي أرشيتيكتو بيتاي فيتاي ديكاتا سيونت أكسبليكابو. نيمو أنيم أبسام فوليوباتاتيم كيواي    فوليوبتاس سايت أسبيرناتشر أيوت أودايت أيوت فيوجايت, سيد كيواي كونسيكيونتشر ماجنايدولارس أيوس كيواي راتاشن فوليوبتاتيم سيكيواي نيسكايونت. نيكيو بوررو كيوايسكيومايست,كيواي دولوريم ايبسيوم كيوا دولار سايت أميت, كونسيكتيتيور,أديبايسكاي فيلايت, سيدكيواي نون نيومكيوام ايايوس موداي تيمبورا انكايديونت يوت لابوري أيت دولار ماجنامألايكيوام كيوايرات فوليوبتاتيم. يوت اينايم أد مينيما فينيام, كيواس نوستريوم أكسيركايتاشيميلامكوربوريس سيوسكايبيت لابورايوسام, نايساي يوت ألايكيوايد أكس أيا كومودايكونسيكيواتشر؟ كيوايس أيوتيم فيل أيوم أيوري ريبريهينديرايت كيواي ان إيا فوليوبتاتي   فيلايت ايسسي كيوم نايهايل موليستايا كونسيكيواتيو,فيلايليوم كيواي دولوريم أيوم فيوجايات كي",img:"/images/book.jpg",title:"مغامرات شرلوك هولمز",publishedDate:"15/09/2020",publishingHouse:"دار البدر للنشر والتوزيع"})
+        const params = {
+          slidesPerView: 'auto',
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: false
+          },
+       }   
+
+        
     return (
        <ResearcherLayout>
            <div className={classes.libraryContainer}>
@@ -48,15 +63,17 @@ export default function bookItemPage() {
                                 </IconButton>
                             </h3>
                             <div className={classes.list}>
-                                <img src={book.img} alt="" />
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                
+                                <Swiper {...params}>
+
+                                    <img src={book.img} alt="" />
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                </Swiper>                                
                             </div>
                         </div>
                         <div className={classes.sideMenu}>
@@ -67,15 +84,16 @@ export default function bookItemPage() {
                                 </IconButton>
                             </h3>
                             <div className={classes.list}>
-                                <img src={book.img} alt="" />
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                <img src={book.img} alt=""/>
-                                
+                                <Swiper {...params}>
+                                    <img src={book.img} alt="" />
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                    <img src={book.img} alt=""/>
+                                </Swiper>
                             </div>
                         </div>
                     </div>
