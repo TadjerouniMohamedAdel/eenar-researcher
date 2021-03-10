6b6dd5cace77b70547113f7b2c998ba8e38e743b
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-03-10 12:00:03 +0100

    transorm project and post view item from client-side to stati generation fetching data

--- pages/researcher/account/posts/[id].js
+++ pages/researcher/account/posts/[id].js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { datagroups, dataarticles } from "../../../../utils/fixtures/DevData";
 import MyHead from "../../../../components/MyHead/MyHead";
 import classes from "../../../../styles/MyPosts.module.css";
@@ -9,42 +9,61 @@ import axios from "axios";
 import moment from "moment";
 import ResearcherLayout from "../../../../layouts/ResearcherLayout/ResearcherLayout";
 import ResearchView from "../../../../components/ResearchView/ResearchView";
-import ResearchViewSkeleton from "../../../../components/ResearchView/ResearchViewSkeleton";
-import {useRouter} from 'next/router'
 
-export default function post() {
-  const [articles, setArticles] = useState(dataarticles);
-  const [groups, setGroups] = useState(datagroups);
-  const [isLoading,setIsLoading] = useState(true)
-  const [research,setResearch] = useState(null)
-  const researchId = useRouter().query.id
-  moment.locale("ar-dz");
 
-  useEffect(() => {
-    researchId && axios({
-      method: "get",
-      url: `${process.env.NEXT_PUBLIC_API_URL}/researcher/postByid?id=${researchId}`,
-    })
+
+export async function getStaticPaths() {
+  let paths = []
+  await  axios({
+              method: "get",
+              url: `${process.env.NEXT_PUBLIC_API_URL}/researcher/post/all`,
+      })
       .then((response) => {
-        setResearch(response.data)
-        setIsLoading(false)
-        console.log("response add", response.data);
+         paths = response.data.map((item)=>{
+              return {
+                  params:{id:item.id.toString()}
+              }
+          })
       })
       .catch((error) => console.log(error));
-  },[])
+  
+  
+    return {
+      paths,
+      fallback: 'blocking' // See the "fallback" section below
+    };
+}
 
+
+export async function getStaticProps(context) {
+  let research =null
+  console.log(context)
+  await axios({
+        method: "get",
+        url: `${process.env.NEXT_PUBLIC_API_URL}/researcher/postByid?id=${context.params.id}`,
+      })
+        .then((response) => {
+            research = response.data
+        })
+        .catch((error) => console.log(error));
+  return {
+    props: {
+      research,
+    }, 
+  }
+}
+
+export default function post({research}) {
+  const [articles, setArticles] = useState(dataarticles);
+  const [groups, setGroups] = useState(datagroups);
+  moment.locale("ar-dz");
   return (
     <ResearcherLayout>
       <MyHead title="الملف الشخصي  - منشوراتي" />
       <div className={classes.myPostsContainer}>
         <div className={classes.mainSection}>
-              {
-                isLoading
-                ?
-                <ResearchViewSkeleton />
-                :
                 <ResearchView  research={research}/>
-              }
+              
         </div>
         <div className={classes.sideSection}>
           <LearnNow />
