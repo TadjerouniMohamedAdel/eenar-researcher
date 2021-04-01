dec3787b1e08825a40d1255af9ac590a15ce9220
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-04-01 09:41:49 +0100

    animation researcherLayout routes v0

--- layouts/ResearcherLayout/ResearcherLayout.js
+++ layouts/ResearcherLayout/ResearcherLayout.js
@@ -2,16 +2,23 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import classes from './ResearcherLayout.module.css'
+import { motion } from 'framer-motion'
 
+const easing = [0.6, -0.05, 0.01, 0.99];
+const animLayout= {
+    initial: { x: "-50vw", opacity: 0,transition:{ease:easing,duration: 0.6,delay:0.6}},
+    animate: { x: 0, opacity: 1,transition:{ease:easing,duration: 0.6,delay:0.8}},
+  };
+  
 export default function ResearcherLayout(props) {
     return (
         <div className={classes.mainLayoutContainer}>
         <Navbar />
        <div className={classes.mainLayoutBaseContent}>
            <Sidebar />
-           <main className={classes.mainContent}>
+           <motion.main className={classes.mainContent} variants={animLayout} exit="initial" initial="initial" animate="animate">
                {props.children}
-           </main>
+           </motion.main>
            
        </div>
     </div>
