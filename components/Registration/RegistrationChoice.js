b58575919e496e2a2972844f556787c529e53f4e
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-04-08 09:31:22 +0100

    registration design improvement

--- components/Registration/RegistrationChoice.js
+++ components/Registration/RegistrationChoice.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import {useEffect} from 'react'
 import { Button, Paper } from '@material-ui/core'
 import classes from '../../styles/Registration.module.css'
 import { useRouter } from 'next/router'
-
+import lottie from 'lottie-web'
+import Link from 'next/link'
 
 export default function RegistrationChoice({choice}) {
     const router = useRouter()
@@ -11,25 +12,37 @@ export default function RegistrationChoice({choice}) {
         router.push(choice.link)
     }
     
+
+    useEffect(()=>{
+        lottie.loadAnimation({
+            container: document.getElementById(`anim-${choice.type}`), // the dom element that will contain the animation
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: choice.img // the path to the animation json
+          });
+    },[])
     
     return (
-        <Paper className={classes.registrationChoiceItem}>
-            <div className={classes.choiceImg}>
-                <img src={choice.img} />
-            </div>
-            <h2 className={classes.choiceTitle}>{choice.title}</h2>
-            <p className={classes.choiceDescription}>
-                {choice.description}
-            </p>
-            <div className={classes.hDivider}></div>
-            <Button
-                variant="contained"
-                onClick={handleClickChoice}
-                className={[classes.choiceButton,choice.type=="center" && classes.centerButton]}
-            >
-                <span>{choice.buttonLabel}</span>
-            </Button>
-        </Paper>
+        <Link href={choice.link}>
+            <Paper className={classes.registrationChoiceItem}>
+                <div className={classes.choiceImg}>
+                    <div id={`anim-${choice.type}`}></div>
+                </div>
+                <h2 className={classes.choiceTitle}>{choice.title}</h2>
+                <p className={classes.choiceDescription}>
+                    {choice.description}
+                </p>
+                <div className={classes.hDivider}></div>
+                <Button
+                    variant="contained"
+                    onClick={handleClickChoice}
+                    className={[classes.choiceButton,choice.type=="center" && classes.centerButton]}
+                    >
+                    <span>{choice.buttonLabel}</span>
+                </Button>
+            </Paper>
+        </Link>
     )
 }
 
--- pages/registration/index.js
+++ pages/registration/index.js
@@ -3,7 +3,7 @@ import RegistrationChoice from '../../components/Registration/RegistrationChoice
 import RegistartionLayout from '../../layouts/Registration/RegistrationLayout'
 const choices =[ 
     {
-        "img":"/images/researcher_registration.png",
+        "img":'/animations/researcher.json',
         "title":"أريد نشر أبحاثي",
         "description":"خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين",
         "type":"individual",
@@ -11,7 +11,7 @@ const choices =[
         "link":"/registration/researcher",
     },
     {
-        "img":"/images/research_center_registration.png",
+        "img":'/animations/center.json',
         "title":"مركز أبحاث ودراسات",
         "description":"خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين",
         "type":"center",
