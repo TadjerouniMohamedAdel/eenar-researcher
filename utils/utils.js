c3eacf38a8e63b51cba4e244cd2f23c66d25ab00
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-04-22 13:03:21 +0100

    add links extractor as functional util

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 export const linksExtractor = (text)=>{
     const expression = /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
-    return input.match(expression);
+    return text.match(expression);
        
 }
