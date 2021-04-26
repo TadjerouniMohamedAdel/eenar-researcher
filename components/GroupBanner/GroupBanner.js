65c72a3e0e7b97d44a61f48a0778c1cb8e836f00
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-04-26 14:21:48 +0100

    edit delete group menu v0

--- components/GroupBanner/GroupBanner.js
+++ components/GroupBanner/GroupBanner.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import { useState } from 'react'
 import classes from './GroupBanner.module.css'
 import PublicIcon from '@material-ui/icons/Public';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const Rectangles = () => (
     <div className={classes.rectangleWhite}>
         <div className={classes.rectangleBlue}></div>
     </div>
 )
+
 export default function GroupBanner() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div className={classes.groupBanner}>
             <div className={classes.bondeau}></div>
             <div className={classes.groupInfo}>
                 <Rectangles />
                 <div className={classes.infoRow}>
-                <div className={classes.info}>
-                    <h1>المجموعة الفلانية</h1>
-                    <h3>أهلا بالباحثين في المجال الفلاني</h3>
-                </div>
+                    <div className={classes.info}>
+                        <h1>المجموعة الفلانية</h1>
+                        <h3>أهلا بالباحثين في المجال الفلاني</h3>
+                    </div>
                     <div className={classes.statsAndActions}>
 
                         <div className={classes.stats}>
@@ -57,9 +71,24 @@ export default function GroupBanner() {
                             <IconButton className={classes.addToGroup} onClick={() => { }}>
                                 <AddIcon />
                             </IconButton>
-                            <IconButton className={classes.editGroup} onClick={() => { }}>
+                            <IconButton className={classes.editGroup} onClick={handleClick}>
                                 <MoreHorizOutlinedIcon />
                             </IconButton>
+                            <Menu
+                                id="group-menu"
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem  onClick={handleClose}>
+                                    تعديل المنشور
+                                    </MenuItem>
+                                <MenuItem  onClick={handleClose}>
+                                    حذف المنشور
+                                    </MenuItem>
+
+                            </Menu>
                         </div>
                     </div>
 
