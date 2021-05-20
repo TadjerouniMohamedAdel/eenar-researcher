265187ec09c6709e866dffa615cfd45e688c5a9d
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-05-20 13:45:16 +0100

    file validation

--- utils/Validation/ValidationObjects.js
+++ utils/Validation/ValidationObjects.js
@@ -378,7 +378,7 @@ export const postSchemaStep2 = Yup.object().shape({
     arabicDescription: Yup.string().required('يجب ملئ هذه المعلومة'),
     englishDescription: Yup.string().required('يجب ملئ هذه المعلومة'),
     publishedDate: Yup.string().required('يجب ملئ هذه المعلومة'),
-    // file: Yup.string().required('يجب ملئ هذه المعلومة'),
+    file: Yup.mixed().required('يجب ملئ هذه المعلومة'),
     // keywords: Yup.string().required('يجب ملئ هذه المعلومة'),
 })
 
