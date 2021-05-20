91576c3116aeaae4ff6460ed827370a9e0da7cbc
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-05-20 16:12:11 +0100

    init messages

--- pages/researcher/messages/index.js
+++ pages/researcher/messages/index.js
@@ -3,7 +3,37 @@ import MyHead from '../../../components/MyHead/MyHead'
 import WorkInProgress from '../../../components/WorkInProgress/WorkInProgress'
 import ResearcherLayout from '../../../layouts/ResearcherLayout/ResearcherLayout'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import BannerMenu from '../../../components/BannerMenu/BannerMenu'
+import classes from '../../../styles/Messages.module.css'
 
+
+const lastMesages = [
+  {
+    "sender":"زوبير ولقبو",
+    "from":"منذ 29 دقيقة",
+    "lastMessages":"جيد جدا، سنقوم بعقد اجتماع معهم في المكان.."
+  },
+  {
+    "sender":"بلال بوعيشة",
+    "from":"منذ 29 دقيقة",
+    "lastMessages":"جيد جدا، سنقوم بعقد اجتماع معهم في المكان.."
+  },
+  {
+    "sender":"إلياس بوجلطية",
+    "from":"منذ 29 دقيقة",
+    "lastMessages":"جيد جدا، سنقوم بعقد اجتماع معهم في المكان.."
+  },
+  {
+    "sender":"جلال الدين شعبان",
+    "from":"منذ 29 دقيقة",
+    "lastMessages":"جيد جدا، سنقوم بعقد اجتماع معهم في المكان.."
+  },
+  {
+    "sender":"أسامة الديزاينر",
+    "from":"منذ 29 دقيقة",
+    "lastMessages":"جيد جدا، سنقوم بعقد اجتماع معهم في المكان.."
+  },
+]
 
 
 export const getStaticProps = async ({ locale }) => ({
@@ -15,7 +45,33 @@ export default function index() {
     return (
         <ResearcherLayout>
             <MyHead title="الرسائل" />
-            <WorkInProgress menu="الرسائل"/>
+            {/* <WorkInProgress menu="الرسائل"/> */}
+            <div className={classes.messagesContainer}>
+              <BannerMenu
+                title="الرسائل"
+                description="تفاعل بشكل أفضل مع زملائك الباحثين، أرسل ملفات، صور وروابط."
+                imgSrc="/images/messages-banner.png"
+              />
+              <h1>الرسائل</h1>
+              <div className={classes.messagesBox}>
+                <div className={classes.lastMessages}>
+                      <div className={classes.lastMessage}>
+                          <div className={classes.lastMessageSenderImage}></div>
+                          <div className={classes.messageInfo}>
+                              <h2>
+                                زوبير ولقبو
+                                <span>
+                                منذ 29 دقيقة
+                                </span>
+                              </h2>
+                          </div>
+                      </div>
+                </div>
+                <div className={classes.divider}></div>
+                <div className={classes.discussion}>
+                </div>
+              </div>
+            </div>
         </ResearcherLayout>
     )
 }
