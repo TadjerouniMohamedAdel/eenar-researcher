5d6a88760cf156aa31c2aba9b04ac5eeede1731b
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-06-01 12:09:54 +0100

    init score screen & ScoreStatCard

--- pages/researcher/account/score/index.js
+++ pages/researcher/account/score/index.js
@@ -3,7 +3,8 @@ import ResearcherAccountLayout from '../../../../layouts/ResearcherAccountLayout
 import WorkInProgress from '../../../../components/WorkInProgress/WorkInProgress'
 import MyHead from '../../../../components/MyHead/MyHead'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
+import classes from '../../../../styles/Score.module.css'
+import ScoreStatCard from '../../../../components/ScoreStatCard/ScoreStatCard'
 
 
 export const getStaticProps = async ({ locale }) => ({
@@ -11,11 +12,82 @@ export const getStaticProps = async ({ locale }) => ({
     ...await serverSideTranslations(locale, ["sidebar"]),
   },
 })
+const generalScoreStats = [
+  {
+    title:"المنشورات",
+    unit:"%",
+    value:80,
+    color:"red",
+    description:"لوريم ايبسوم هو نموذج افتراضي يوضع في التصاميم لتعرض على",
+    offset:{
+      increase:true,
+      value:8.2
+    }
+  },
+  {
+    title:"النقاشات",
+    unit:"",
+    value:"4:39",
+    color:"orange",
+    description:"لوريم ايبسوم هو نموذج افتراضي يوضع في التصاميم لتعرض على",
+    offset:{
+      increase:false,
+      value:2.4
+    }
+  },
+  {
+    title:"القراءات",
+    unit:"",
+    value:262,
+    color:"green",
+    description:"لوريم ايبسوم هو نموذج افتراضي يوضع في التصاميم لتعرض على",
+    offset:{
+      increase:true,
+      value:3.6
+    }
+  },
+  {
+    title:"التحميلات",
+    unit:"",
+    value:71,
+    color:"blue",
+    description:"لوريم ايبسوم هو نموذج افتراضي يوضع في التصاميم لتعرض على",
+  },
+  {
+    title:"التوصيات",
+    unit:"%",
+    value:80,
+    color:"black",
+    description:"لوريم ايبسوم هو نموذج افتراضي يوضع في التصاميم لتعرض على",
+    offset:{
+      increase:true,
+      value:8.2
+    }
+  }
+]
 export default function index() {
   return (
     <ResearcherAccountLayout>
       <MyHead title="الملف الشخصي  - الرصيد" />
-      <WorkInProgress menu="الرصيد" />
+      {/* <WorkInProgress menu="الرصيد" /> */}
+      <div className={classes.scoreContainer}>
+          <div className={classes.scoreGeneralStats}>
+              {
+                generalScoreStats.map((score,index)=>(
+                  <ScoreStatCard
+                    key={`index=${index}`}  
+                    score={score}
+                    />
+                ))
+              }
+          </div>
+          {/* <div className={classes.monthStats}>
+              
+          </div> */}
+          {/* <div className={classes.rowStats}>
+              
+          </div> */}
+      </div>
     </ResearcherAccountLayout>
   )
 }
