b4a12935e998c59642894c1a9c406b67f04f2a97
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-07-07 14:26:31 +0100

    update aboutGroup with typescript & storybook

--- components/AboutGroup/AboutGroup.tsx
+++ components/AboutGroup/AboutGroup.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
 import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
 import { IconButton } from '@material-ui/core';
 import classes from './AboutGroup.module.css'
-import { dataaboutme } from '../../utils/fixtures/DevData'
-import PropTypes from 'prop-types';
 import { format} from 'date-fns'
 import arLocale  from 'date-fns/locale/ar-DZ'
+import { GroupCardProps } from '../../utils/types/types';
 
-
-export default function AboutGroup({group}) {
-    const [aboutme, setAboutme] = useState(dataaboutme)
-
+const AboutGroup:React.FC<GroupCardProps>=({group})=> {
     return (
         <div className={classes.aboutGroup}>
             <h2>
@@ -23,14 +19,11 @@ export default function AboutGroup({group}) {
                 {group.description}
             </p>
             <ul className={classes.aboutGroupInfo}>
-                <li><span className={classes.infoLabel}>الموقع</span><span className={`${classes.infoValue} ${classes.website}`}><a href={aboutme.website} target="_blank">{group.website}</a></span></li>
+                <li><span className={classes.infoLabel}>الموقع</span><span className={`${classes.infoValue} ${classes.website}`}><a href={group.webSite || ""} target="_blank">{group.webSite}</a></span></li>
                     <div className={classes.divider}></div>
                 <li><span className={classes.infoLabel}> الإنضمام للمجموع</span><span className={classes.infoValue}>{format(new Date(),"dd MMMM yyyy",{locale:arLocale })}</span></li>
             </ul>
         </div>
     )
 }
-
-AboutGroup.propTypes = {
-    group:PropTypes.object.isRequired
-}
+export default AboutGroup
